690361f0c078bbf587e34c164b3a3a11f755bdc7
GabrielSDesiderio <desiderio.gabriel@protonmail.com>
2021-02-10 15:10:52 +0100

    Create Ceaser Cipher at ROT13.js

---
+++ Ceaser Cipher at ROT13.js
@@ -0,0 +1,17 @@
+//Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+function rot13(str) {
+  var charArray = [];
+  var regExp = /[A-Z]/;
+  str = str.split("");
+        /* A more general approach, possible because of modular arithmetic and cyclic nature of rot13 transform*/
+  for (var i in str) {
+    if (regExp.test(str[i])) {
+      charArray.push(((str[i].charCodeAt() - 65 + 13) % 26) + 65);
+    } else {
+      charArray.push(str[i].charCodeAt());
+    }
+  }
+  str = String.fromCharCode.apply(String, charArray);
+  return str;
+}
