9b81c239bfa5ed6ab5c181f330e7271a0d5be334
GabrielSDesiderio <desiderio.gabriel@protonmail.com>
2021-02-10 15:00:43 +0100

    Personal Js Portfolio
    JavaScript Algorithms and Data Structures Projects

--- Roman Number Converter.js
+++ Roman Number Converter.js
@@ -1,13 +1,13 @@
 function convertToRoman(num) {
-    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var decimalValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var romanValue = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
     var romanized = "";
 
 
-    for (var i = 0; i < decimalValue.length; i++) {
-        while (decimalValue[i] <= num) {
+    for (var i = 0; i < decimalValues.length; i++) {
+        while (decimalValues[i] <= num) {
         romanized += romanValue[i];
-        num -= decimalValue[i];
+        num -= decimalValues[i];
         }
     }
 
